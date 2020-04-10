57eb01295b04bb36d7503aa6b37d592209b137e3
luddet04 <ludvig.magnusson04@gmail.com>
2020-04-10 18:51:30 +0200

    changed port

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const Milena = require("./Milena/app")();
 app.use("/milena", Milena.app);
 //app.use("/lud-dictionary", LudDictionary.app)
 
+const port = process.env.app_port || 8080;
+
 app.listen(4000, () => console.log("LudvigDB main server running on port 4000"));
